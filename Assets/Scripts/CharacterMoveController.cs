13b2495d0caadf806d4b7f6c5ff73e1b5c10d8fe
PC\Shergat <cbellom@unal.edu.co>
2017-01-17 01:02:57 -0500

    character controler move v2

--- Assets/Scripts/CharacterMoveController.cs
+++ Assets/Scripts/CharacterMoveController.cs
@@ -1,21 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof (Animator))]
+[RequireComponent (typeof (CharacterController))]
 public class CharacterMoveController : MonoBehaviour {
 
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
-    private float xMove = 0;
-    private float zMove = 0;
-    private Vector3 moveDirection = Vector3.zero;
+    private float horizontalInput = 0;
+    private float verticalInput = 0;
 
-	CharacterController controller;
+	private Vector3 moveDirection = Vector3.zero;
+	private Vector3 movement = Vector3.zero;
+	private Vector3 forward = Vector3.zero;
+	private Vector3 right = Vector3.zero;
+	private Vector3 targetDirection = Vector3.zero;
+
+	private CharacterController controller;
 	private Animator playerAnimatior;
 
 	void Start () {
-		controller = GetComponent<CharacterController>();
 		playerAnimatior = GetComponent<Animator> ();
+		controller = GetComponent<CharacterController>();
 	}
 
 	void FixedUpdate () {
@@ -30,19 +37,32 @@ public class CharacterMoveController : MonoBehaviour {
 				Jump ();
 
         }
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+		movement.y -= gravity * Time.deltaTime;
+		controller.Move(movement * Time.deltaTime);
     }
 
 	void Move(){
-		xMove = Input.GetAxis("Horizontal");
-		zMove = Input.GetAxis("Vertical");
+		forward = transform.forward;
+		right = transform.right;
 
-		moveDirection = new Vector3(xMove, 0, zMove);
-		moveDirection = transform.TransformDirection(moveDirection);
-		moveDirection *= speed;
+		horizontalInput = Input.GetAxis("Horizontal");
+		verticalInput = Input.GetAxis("Vertical");
 
-		playerAnimatior.SetFloat ("moveSpeed", Mathf.Abs (zMove));	
+		targetDirection = horizontalInput * right + verticalInput * forward;
+		Rotate ();
+		MoveForward ();
+
+		playerAnimatior.SetFloat ("moveSpeed", controller.velocity.magnitude);	
+	}
+
+	void Rotate(){
+		if(targetDirection != Vector3.zero)
+			transform.rotation = Quaternion.LookRotation (moveDirection);
+	}
+
+	void MoveForward(){
+		moveDirection = Vector3.RotateTowards (moveDirection, targetDirection, 100 * Mathf.Deg2Rad * Time.deltaTime, 1000);
+		movement = moveDirection * speed;
 	}
 
 	void CheckGrounded (){
@@ -52,6 +72,6 @@ public class CharacterMoveController : MonoBehaviour {
 
 	void Jump(){
 		playerAnimatior.SetBool ("grounded", false);	
-		moveDirection.y = jumpSpeed;
+		movement.y = jumpSpeed;
 	}
 }
