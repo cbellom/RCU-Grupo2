f1192ad8fb5236c20b7dbf6e5407beeea0850bbc
idea-PC\Shergat <cbellom@unal.edu.co>
2017-01-17 16:58:18 -0500

    Add ui: main menu, help messages

---
+++ Assets/Scripts/UI/HelpMessageController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof (CanvasGroup))]
+public class HelpMessageController : MonoBehaviour {
+
+    [SerializeField]
+    private float fadeSpeed;
+    [SerializeField]
+    private float secondsDuration;
+    [SerializeField]
+    private Text helpLabel;
+    [SerializeField]
+    private bool isTurnOn = false;
+    private CanvasGroup canvasGroup;
+
+    private void Awake() {
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0;
+    }
+
+    private void Update() {
+        if (isTurnOn && canvasGroup.alpha < 1)
+            canvasGroup.alpha += Time.deltaTime * fadeSpeed;
+        else if (!isTurnOn && canvasGroup.alpha > 0)
+            canvasGroup.alpha -= Time.deltaTime * fadeSpeed;
+
+#if UNITY_EDITOR
+        if (Input.GetKey(KeyCode.H))
+            ShowNewMessage("TEST: Editor help message");
+#endif
+    }
+
+    public void ShowNewMessage(string message) {
+        helpLabel.text = message;
+        StopAllCoroutines();
+        StartCoroutine(ShowHelp(secondsDuration));
+    }
+
+    private IEnumerator ShowHelp(float seconds)    {
+        isTurnOn = true;
+
+        yield return new WaitForSeconds(seconds);
+
+        isTurnOn = false;
+    }
+    
+}
---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+
+    [SerializeField]
+    private float fadeSpeed;
+    [SerializeField]
+    private bool isTurnOn = false;
+    private CanvasGroup canvasGroup;
+
+    private void Awake() {
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0;
+    }
+
+    private void Update()
+    {
+        if (isTurnOn && canvasGroup.alpha < 1)
+            canvasGroup.alpha += Time.deltaTime * fadeSpeed;
+        else if (!isTurnOn && canvasGroup.alpha > 0)
+            canvasGroup.alpha -= Time.deltaTime * fadeSpeed;
+
+#if UNITY_EDITOR
+        if (Input.GetKey(KeyCode.M))
+            isTurnOn = true;
+#endif
+    }
+
+    public void ChangeScene(string sceneName) {
+        Debug.Log("Load scene " + sceneName);
+    }
+}
