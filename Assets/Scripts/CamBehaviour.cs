feffac64fc2a249a006c172c490c5cf59c6f5407
watachi90 <watachi90@gmail.com>
2017-01-16 19:21:06 -0500

    script for cam behaviour

---
+++ Assets/Scripts/CamBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CamBehaviour : MonoBehaviour {
+
+	public Transform character;
+	public float min_distance;
+	public float height;
+
+	void Start() {
+		min_distance = 2;
+	}
+	void Update () {
+		Vector3 target_pos = character.position * 2;
+		if (target_pos.magnitude < min_distance)
+			target_pos = target_pos.normalized * min_distance;
+		target_pos.y = target_pos.y + height;
+		transform.position = transform.position + (target_pos - transform.position) / 10;
+		transform.LookAt(character);
+
+	}
+}
