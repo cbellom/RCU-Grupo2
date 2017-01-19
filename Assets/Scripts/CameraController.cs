3eb591a23ccaf464f3a7de4434eb25e6379e8610
PC\Shergat <cbellom@unal.edu.co>
2017-01-18 23:30:55 -0500

    fix camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,15 +2,22 @@
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
+	public float heightOffset = 5;
+	public float speedMod = 10.0f;
+	public Transform target;
+	public Vector3 offset;
+	private Vector3 cameraPosition;
 
-    public GameObject player;
-    private Vector3 offset;
+	void Start () {
+		cameraPosition = Vector3.zero;
+		transform.LookAt(target);
+	}
 
-    void Start() {
-        offset = transform.position - player.transform.position;
-    }
-
-    void LateUpdate() {
-        transform.position = player.transform.position + offset;
-    }
+	void Update () {
+		cameraPosition.x = target.position.x * offset.x; 
+		cameraPosition.y = target.position.y + offset.y; 
+		cameraPosition.z = target.position.z * offset.z; 
+		transform.position = cameraPosition;
+		transform.LookAt (target);
+	}
 }
