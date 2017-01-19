8dc755bcb660eb0e647848cdb99ba89ca445fb4e
PC\Shergat <cbellom@unal.edu.co>
2017-01-18 23:58:58 -0500

    Create class to to intaractuate with objects on trigger colliders

---
+++ Assets/Scripts/InteractableObjects/ExampleObjectTrap.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleObjectTrap : InteractableObject {
+
+	void Awake() {
+		ObjectActivated += HandleExampleObjectActived;
+		ObjectFinished += HandleExampleObjectExpired;
+	}
+
+	private void HandleExampleObjectActived() {
+		SomethigHappensOnActivate ();
+		StopAllCoroutines ();
+		StartCoroutine(WaitFinishAction(timeToTriggerAction));
+	}
+
+	private void HandleExampleObjectExpired(GameObject player) {
+		SomethigHappensOnOver ();
+		Destroy(gameObject);
+	}
+
+	private void SomethigHappensOnActivate() {
+		Debug.Log ("You active this trap!! The limit is your imagination");
+	}
+
+	private void SomethigHappensOnOver() {
+		Debug.Log ("Trap is Over!! The limit is your imagination");
+	}
+}
---
+++ Assets/Scripts/InteractableObjects/InteractableObject.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class InteractableObject : MonoBehaviour {
+
+	public bool isAutoActivated = true;
+	public float timeToTriggerAction = 0;
+	protected GameObject player;
+
+	public Action ObjectActivated
+	{
+		get;
+		set;
+	}
+
+	public Action<GameObject> ObjectFinished
+	{
+		get;
+		set;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (isAutoActivated && other.gameObject.CompareTag("Player"))
+		{
+			player = other.gameObject;
+			ActiveObject();
+		}
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (!isAutoActivated && other.gameObject.CompareTag("Player"))
+		{
+			if(Input.GetKey(KeyCode.E))
+			{
+				player = other.gameObject;
+				this.GetComponent<Collider> ().enabled = false;
+				ActiveObject();
+			}
+		}
+	}
+
+	protected IEnumerator WaitFinishAction(float time)
+	{
+		yield return new WaitForSeconds(time);
+
+		if (ObjectFinished != null)
+			ObjectFinished(player);
+	}
+
+	private void ActiveObject()
+	{
+		if (ObjectActivated != null)
+			ObjectActivated();		
+	}
+}
