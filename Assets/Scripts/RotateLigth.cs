c73574db470c23ed5c88223f20d4d6b7a5d49f80
Estudiante <Estudiante@unad.edu.co>
2017-01-18 21:59:48 -0500

    Post processing

---
+++ Assets/Scripts/RotateLigth.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateLigth : MonoBehaviour {
+	public float rotationSpeed;
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3(0,rotationSpeed * Time.deltaTime, 0));
+	}
+}
