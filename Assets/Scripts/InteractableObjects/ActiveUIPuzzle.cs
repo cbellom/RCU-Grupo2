30a1a0b0b0b829bb3b192db1ed3ffc53107ed171
Estudiante <Estudiante@unad.edu.co>
2017-01-19 21:40:38 -0500

    Create hack puzzle

---
+++ Assets/Scripts/InteractableObjects/ActiveUIPuzzle.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActiveUIPuzzle : InteractableObject {
+	
+	public UIPuzzle puzzle;
+	public Camera cameraPuzzle;
+	private Camera mainCamera;
+
+	void Awake() {
+		mainCamera = GameObject.Find ("Main Camera").GetComponent<Camera>();
+		ObjectActivated += HandleExampleObjectActived;
+		ObjectFinished += HandleExampleObjectExpired;
+	}
+
+	private void HandleExampleObjectActived() {
+		ActivePuzze ();
+		StopAllCoroutines ();
+		StartCoroutine(WaitFinishAction(timeToTriggerAction));
+	}
+
+	private void HandleExampleObjectExpired(GameObject player) {
+		Destroy(gameObject);
+	}
+
+	private void ActivePuzze() {
+		Debug.Log ("Hola");
+		LockPlayerMove ();
+		SwitchCameras ();
+		puzzle.Active();
+	}
+
+	private void SwitchCameras(){
+		mainCamera.enabled = false;
+		cameraPuzzle.enabled = true;
+	}
+
+	private void LockPlayerMove(){
+		player.GetComponent<CharacterMoveController> ().enabled = false;
+	}
+}
---
+++ Assets/Scripts/Puzzles/UIPuzzle.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIPuzzle : MonoBehaviour {
+
+	public void Active(){
+		
+	}
+}
