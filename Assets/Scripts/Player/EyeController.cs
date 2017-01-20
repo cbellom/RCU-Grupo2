4924816e0e8ac09d9e8759b5059653879c52da34
Estudiante <Estudiante@unad.edu.co>
2017-01-19 20:04:59 -0500

    Eye animations

---
+++ Assets/Scripts/Player/EyeController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeController : MonoBehaviour {
+
+	public Animator animator;
+
+	void Start (){
+		animator = GetComponent<Animator>();
+	}
+	void Update (){
+		float moveX = Input.GetAxis ("Horizontal");
+		animator.SetFloat ("moveOnX", moveX);
+	}
+
+
+}
