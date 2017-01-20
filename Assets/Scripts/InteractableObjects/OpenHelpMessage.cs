ba0f1031a3856ad5104007e48b70da7ea9b88470
Estudiante <Estudiante@unad.edu.co>
2017-01-19 19:17:51 -0500

    Help message animation. Eye animation. Active Help message

---
+++ Assets/Scripts/InteractableObjects/OpenHelpMessage.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenHelpMessage : InteractableObject {
+	public string message;
+	public HelpMessageController helpMessage;
+
+	void Awake() {
+		ObjectActivated += HandleExampleObjectActived;
+		ObjectFinished += HandleExampleObjectExpired;
+	}
+
+	private void HandleExampleObjectActived() {
+		OpenMessage ();
+		StopAllCoroutines ();
+		StartCoroutine(WaitFinishAction(timeToTriggerAction));
+	}
+
+	private void HandleExampleObjectExpired(GameObject player) {
+		Destroy(gameObject);
+	}
+
+	private void OpenMessage() {
+		Debug.Log ("Open Help");
+		if (helpMessage != null) {
+			helpMessage.ShowNewMessage (message);
+		}
+	}
+
+}
