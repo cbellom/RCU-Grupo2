ce6869a3f0d572d1a7c33b59e0a0d2879b5b12ca
Estudiante <Estudiante@unad.edu.co>
2017-01-23 22:06:14 -0500

    cinematics added

---
+++ Assets/Scripts/InteractableObjects/PlayCinematic.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayCinematic : InteractableObject {
+	public int cinematicNumber;
+	public CinematicsController cinematics;
+
+	void Awake() { 	
+		ObjectActivated += HandleObjectActived;
+		ObjectFinished += HandleObjectExpired;
+	}
+
+	private void HandleObjectActived() {
+		Play ();
+		StopAllCoroutines ();
+		StartCoroutine(WaitFinishAction(timeToTriggerAction));
+	}
+
+	private void HandleObjectExpired(GameObject player) {
+		Stop ();
+	}
+
+	private void Stop (){
+		cinematics.gameObject.SetActive (false);
+		cinematics.StopCinematic ();		
+	}
+
+	private void Play() {
+		cinematics.gameObject.SetActive (true);
+		cinematics.PlayCinematic (cinematicNumber);
+	}
+
+}
---
+++ Assets/Scripts/World/CinematicsController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CinematicsController : MonoBehaviour {
+
+	public Animator animator;
+
+	void Start () {
+		animator = GetComponent<Animator> ();
+		if (animator == null)
+			Debug.LogError ("CinematicsController require an animator component");
+	}
+
+	public void PlayCinematic ( int number) {
+		animator.SetInteger ("number", number);
+	}
+	public void StopCinematic () {
+		animator.SetInteger ("number", 0);
+	}
+
+}
